e3b46e93f74eafbc30affe4a57676d75fd5ac47e
Erickbrito1 <51213477+Erickbrito1@users.noreply.github.com>
2019-06-06 15:00:22 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
-function create_anticipation($recipient_id,$build,$payment_date,$requested_amount,$array_payable_ids,$pagarme_api_token){
+function create_anticipation($recipient_id,$build,$payment_date,$array_payables_ids,$pagarme_api_token){
 
     $curl = curl_init();
     //$payment_date_convert= (1559811600*1000);
 
-    $array_payable_ids_string = implode(",",$array_payable_ids);    
+    $array_payables_ids_string = implode(",",$array_payables_ids);    
 
     curl_setopt_array($curl, array(
     CURLOPT_URL => "https://api.pagar.me/1/recipients/".$recipient_id."/bulk_anticipations/",
@@ -15,7 +15,7 @@ function create_anticipation($recipient_id,$build,$payment_date,$requested_amoun
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => "{\r\n    \"build\": \" ". $build . " \", \r\n    \"payment_date\": \"".$payment_date."\", \r\n\t\"payables\": [". $array_payable_ids_string ."],\r\n\t\"timeframe\": \"start\",\r\n\t\"requested_amount\": ". $requested_amount ." \r\n\r\n}",
+    CURLOPT_POSTFIELDS => "{\r\n    \"build\": \" ". $build . " \", \r\n    \"payment_date\": \"".$payment_date."\", \r\n\t\"payables_ids\": [". $array_payables_ids_string ."] \r\n\r\n}",
     CURLOPT_HTTPHEADER => array(
         "Authorization: " . $pagarme_api_token,
         "Cache-Control: no-cache",
